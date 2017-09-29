f857737d7b70556d9e5a129858897234a68f8d87
CvetelinLozanov <spetsnaz@mail.bg>
2017-09-29 12:03:58 +0300

    Add solution for 15. Balanced Brackets i Data types More Exercise

---
+++ Programing Fundamentals C#/Data Types and Variables - More Exercises/15. Balanced Brackets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var brackets = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var str = Console.ReadLine();
+
+                if (str == "(")
+                    brackets++;
+                if (str == ")")
+                    brackets--;
+                if (str == "(" && i == n)
+                {
+                    Console.WriteLine("UNBALANCED");
+                    return;
+                }
+            }
+
+            if (brackets == 0)
+                Console.WriteLine("BALANCED");
+            else if (brackets < 0 || brackets > 0)
+                Console.WriteLine("UNBALANCED");
+        }
+    }
+}
