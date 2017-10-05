37229725d615b493efba6c0b010f9792c91803a9
CvetelinLozanov <spetsnaz@mail.bg>
2017-10-05 15:52:21 +0300

    Add solutions for Arrays Exercise

---
+++ Programing Fundamentals C#/Arrays - Exercises/03. Fold and Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int k = numbers.Length / 4;         
+            int[] firstArr = new int[k];
+            int[] middleArr = new int[2 * k];
+            int[] lastArr = new int[k];
+            int[] resultArr = new int[2 * k];
+            for (int i = 0; i < k; i++)
+            {
+
+            }
+        }
+    }
+}
