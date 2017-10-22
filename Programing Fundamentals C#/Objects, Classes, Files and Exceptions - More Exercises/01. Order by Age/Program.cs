892e5efe21d4afe785e51756e66d660c8bdaf4b5
CvetelinLozanov <spetsnaz@mail.bg>
2017-10-22 11:26:55 +0300

    Add solutions for Objects More exercise

---
+++ Programing Fundamentals C#/Objects, Classes, Files and Exceptions - More Exercises/01. Order by Age/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.Order_by_Age
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Person> people = ReadPeople();
+            List<Person> orderedPeople = people.OrderBy(p => p.Age).ToList();
+            foreach (var person in orderedPeople)
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.Id} is {person.Age} years old.");
+            }
+        }
+
+        private static List<Person> ReadPeople()
+        {
+            List<Person> people = new List<Person>();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person()
+                {
+                    Name = tokens[0],
+                    Id = tokens[1],
+                    Age = int.Parse(tokens[2]),
+                };
+                people.Add(person);
+                command = Console.ReadLine();
+            }
+            return people;
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Programing Fundamentals C#/Objects, Classes, Files and Exceptions - More Exercises/02. Vehicle Catalogue/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Vehicle> vehicles = ReadVehicles();
+           
+            string vehiclesForPrint = Console.ReadLine();
+            List<string> cars = new List<string>();
+            while (vehiclesForPrint != "Close the Catalogue")
+            {
+                cars.Add(vehiclesForPrint);
+                vehiclesForPrint = Console.ReadLine();
+            }
+          
+        }
+
+        private static List<Vehicle> ReadVehicles()
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string type = tokens[0];
+                string model = tokens[1];
+                string color = tokens[2];
+                int horsePower = int.Parse(tokens[3]);
+                Vehicle vehicle = new Vehicle
+                {
+                    Type = type,
+                    Model = model,
+                    Color = color,
+                    HorsePower = horsePower,
+                };
+                vehicles.Add(vehicle);
+                command = Console.ReadLine();
+            }
+            return vehicles;
+        }
+    }
+    public class Cataloge
+    {
+        public List<Vehicle> Vehicles { get; set; }
+    }
+    public class Vehicle
+    {
+        public string Type { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int HorsePower { get; set; }
+    }
+}
